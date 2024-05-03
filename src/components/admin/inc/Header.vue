ea4b6fee22842532b3110bff61dfe08909ab4f57
JiniBean_hi <JiniBean@DESKTOP-2BGN6PD>
2024-05-03 13:09:36 +0900

    component 배우는중

--- src/components/admin/inc/Header.vue
+++ src/components/admin/inc/Header.vue
@@ -2,8 +2,9 @@
   <header>
     <h1>Rland Admin</h1>
     <ul>
-      <li><RouterLink to="/index">Home</RouterLink></li>
+      <li><RouterLink to="/admin/index">Home</RouterLink></li>
       <li><RouterLink to="/menu/list">메뉴</RouterLink></li>
+      <li><RouterLink to="/">사용자 페이지</RouterLink></li>
     </ul>
   </header>
 </template>
